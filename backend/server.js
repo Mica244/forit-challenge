180ce1ec8ac3bd761b28c9172f94bbfe3d7d9ada
Micaela Pincevic <micaelapincevic7@gmail.com>
2026-06-18 08:07:10 -0300

    backend con endpoints CRUD de tareas

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+let tasks = []
+
+app.get('/api/tasks', (req, res) => {
+    res.json(tasks)
+})
+
+app.post('/api/tasks', (req, res) => {
+    const { title, description } = req.body
+    if (!title) {
+    return res.status(400).json({ error: 'El título es obligatorio' })}
+
+    const newTasks = {
+        id: crypto.randomUUID(),
+        title: title,
+        description: description || '',
+        completed: false,
+        createdAt: new Date()
+    }
+    tasks.push(newTasks)
+    res.status(201).json(newTasks)
+})
+
+app.put('/api/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const index = tasks.findIndex(t => t.id === id)
+    if (index === -1 ){
+        res.status(404).json("Elemento no encontrado")
+        return
+    }
+    tasks[index] = { ...tasks[index], ...req.body }
+    res.json(tasks[index])
+})
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const index = tasks.findIndex(t => t.id === id)
+    if (index === -1 ){
+        res.status(404).json("Elemento no encontrado")
+        return
+    }
+    tasks.splice(index, 1)
+    res.status(204).send()
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
