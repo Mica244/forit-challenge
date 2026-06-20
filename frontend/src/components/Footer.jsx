e211ab14072fd1a6c445993607a9425d2149f6c7
Micaela Pincevic <micaelapincevic7@gmail.com>
2026-06-19 21:45:37 -0300

    fix: actualización footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
             <div className="footer-content">
                 <p className="footer-name">Micaela Pincevic</p>
                 <div className="footer-links">
-                    <a href="https://tuportafolio.com" target="_blank">Portfolio</a>
+                    <a href="https://cosmic-swan-f6e5e4.netlify.app/?fbclid=PAT01DUAPu8-lleHRuA2FlbQIxMABzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAacN6VLKHPLexait6jFmnfrfULj_wMPohAV4jkA8Zli9yBoOU62VRYgzDYKJqQ_aem_ZnX_LSslePMOt0p_8AOsOw" target="_blank">Portfolio</a>
                     <a href="https://www.linkedin.com/in/micaela-pincevic-01bbb0255/" target="_blank">LinkedIn</a>
-                    <a href="https://cosmic-swan-f6e5e4.netlify.app/?fbclid=PAT01DUAPu8-lleHRuA2FlbQIxMABzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAacN6VLKHPLexait6jFmnfrfULj_wMPohAV4jkA8Zli9yBoOU62VRYgzDYKJqQ_aem_ZnX_LSslePMOt0p_8AOsOw" target="_blank">GitHub</a>
+                    <a href="https://github.com/Mica244" target="_blank">GitHub</a>
                 </div>
             </div>
         </footer>
